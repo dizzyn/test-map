5241daad9336c7eb86572009ec2192456e70294d
vitekzkytek <vitekzkytek@gmail.com>
2015-09-25 11:13:09 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1727,8 +1727,8 @@ var data = {
             "en": "Luxembourg"
         },
         "pinFix": {
-            "x": -100,
-            "y": -100
+            "x": -50,
+            "y": -50
         },
         "metrics": {
             "EIB": {
